ebd35828b79c379f049e8db85d309af15b0e9798
Bilal Ahmed Mirza <bilalahmedmirza419@gmail.com>
2023-09-18 22:51:01 +0500

    Third Commit.

--- app/src/main/java/com/bilalmirza/xsandos/MainActivity.kt
+++ app/src/main/java/com/bilalmirza/xsandos/MainActivity.kt
@@ -2,12 +2,16 @@ package com.bilalmirza.xsandos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bilalmirza.xsandos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setTheme(R.style.Base_Theme_XsAndOs)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
